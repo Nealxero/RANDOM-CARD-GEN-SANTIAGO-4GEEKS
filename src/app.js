ed130aedb35ce4d20318cad485f3f9c79ed87249
Nealxero <santixero@gmail.com>
2022-06-29 21:06:10 +0000

    todo hecho

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const symbolvariant = randomSymbol();
+  document.querySelector(".topLeft").innerHTML = symbolvariant;
+  document.querySelector(".botRight").innerHTML = symbolvariant;
+  document.querySelector(".number").innerHTML = randomNumb();
+};
+//write your code here
+let randomNumb = () => {
+  let numberList = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numberGen = Math.floor(Math.random() * numberList.length);
+  return numberList[numberGen];
+};
+let randomSymbol = () => {
+  let symbolList = ["♥", "♠", "♣", "♦"];
+  let symbolGen = Math.floor(Math.random() * symbolList.length);
+  return symbolList[symbolGen];
 };
